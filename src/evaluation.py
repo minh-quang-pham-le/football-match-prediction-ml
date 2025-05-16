d2a8f2d19b17b3007378e4a12d189c1e3dcf812a
minh-quang-pham-le <mkw.dsa@gmail.com>
2025-05-16 07:41:25 +0700

    Update

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from sklearn.metrics import accuracy_score, f1_score, roc_auc_score, classification_report
 
-def evaluate_model(model, X_test: pd.DataFrame, y_test: pd.Series):
+def evaluate_model(model, X_test: pd.DataFrame, y_test: pd.Series) -> None:
+    '''Đánh giá mô hình với các chỉ số Accuracy, F1-score, AUC-ROC và Classification Report.'''
+    
     y_pred = model.predict(X_test)
     print("Accuracy:", accuracy_score(y_test, y_pred))
     print("F1-score:", f1_score(y_test, y_pred, average='macro'))
